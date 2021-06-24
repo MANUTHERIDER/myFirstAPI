95d88f3a9b4ce6eb3c16f290b4a6bcc6c5515a2f
riDer <sahumanu021@gmail.com>
2021-06-24 16:15:04 +0530

    small change

--- app.js
+++ app.js
@@ -13,9 +13,19 @@
 
 const express = require('express');
 
+const bookRouter = express();
 const app = express();
 
 const port = process.env.PORT || 2005;
+
+bookRouter.route('/books')
+  .get((req, res) => {
+    const response = { hello: 'This is my API' };
+
+    res.json(response);
+  });
+
+app.use('/api', bookRouter);
 
 app.get('/', (req, res) => {
   res.send('Welcome to my API again');
